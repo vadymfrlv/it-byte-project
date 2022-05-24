e2f72295faf79e1de95a966b51f27c92876a12c4
vadymfrlv <65303465+vadymfrlv@users.noreply.github.com>
2022-05-24 08:52:27 +0000

    Deploying to gh-pages from @ vadymfrlv/it-byte-project@51c7f2cfcd8fa6a534416ad4ba8bcf6a1e23d881 🚀

--- src.69bed8e4.js
+++ src.69bed8e4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/it-byte-project/src.e2c04f8b.js.map
+//# sourceMappingURL=/it-byte-project/src.69bed8e4.js.map
